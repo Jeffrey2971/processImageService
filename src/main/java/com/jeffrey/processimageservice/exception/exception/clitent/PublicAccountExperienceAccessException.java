cebea9fbe7c872dfe8366d6494b21f19b6f6c333
Jeffrey <Jeffrey2971@outlook.com>
2023-03-02 13:12:25 +0800

    定义了公众号体验异常

---
+++ src/main/java/com/jeffrey/processimageservice/exception/exception/clitent/PublicAccountExperienceAccessException.java
@@ -0,0 +1,13 @@
+package com.jeffrey.processimageservice.exception.exception.clitent;
+
+/**
+ * @author jeffrey
+ * @since JDK 1.8
+ */
+
+
+public class PublicAccountExperienceAccessException extends ClientException {
+    public PublicAccountExperienceAccessException(String message) {
+        super(message);
+    }
+}
