e065f29b25c612795bf4730777f5e64f17f5ecc0
Jeffrey <Jeffrey2971@outlook.com>
2023-03-02 15:22:14 +0800

    文件工具类

---
+++ src/main/java/com/jeffrey/processimageservice/utils/FileUtil.java
@@ -0,0 +1,32 @@
+package com.jeffrey.processimageservice.utils;
+
+import com.jeffrey.processimageservice.entities.enums.SupportUploadImageType;
+import org.apache.tika.Tika;
+
+import java.io.IOException;
+
+/**
+ * @author jeffrey
+ * @since JDK 1.8
+ */
+
+
+public class FileUtil {
+
+    /**
+     * 校验给定的 bytes 数组是否为图片且格式为 image/bmp image/jpeg image/png
+     * @param bytes 图片的字节数组
+     * @return 为 null 则表示不支持
+     * @throws IOException ioException
+     */
+    public static SupportUploadImageType getSupportUploadImageType(byte[] bytes) throws IOException {
+
+        String type = new Tika().detect(bytes);
+
+        return "image/png".equals(type)
+                ? SupportUploadImageType.IMAGE_PNG : "image/bmp".equals(type)
+                ? SupportUploadImageType.IMAGE_BMP : "image/jpeg".equals(type)
+                ? SupportUploadImageType.IMAGE_JPEG : null;
+    }
+
+}
