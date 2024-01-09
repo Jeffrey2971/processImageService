08700dd11f6f67cdb15a892577ceedfbb66e95b4
Jeffrey <Jeffrey2971@outlook.com>
2024-01-09 20:37:05 +0800

    新增一些对 order_info 表更新、查询方法

--- src/main/java/com/jeffrey/processimageservice/service/OrderService.java
+++ src/main/java/com/jeffrey/processimageservice/service/OrderService.java
@@ -1,17 +1,24 @@
 package com.jeffrey.processimageservice.service;
 
 
+import com.jeffrey.processimageservice.entities.OrderInfo;
+import com.jeffrey.processimageservice.enums.OrderStatus;
 import com.jeffrey.processimageservice.vo.R;
 
 public interface OrderService {
-//    String createQrCodeToBase64(String content, int width, int height);
     String getOpenId(String code, String state);
+
     String getOpenId();
 
     String getUserIdentifier();
 
     boolean hasOpenIdInSession();
 
-    R processOrder(String orderNo);
+    R processOrder(String orderNo, OrderInfo ignore);
 
+    OrderInfo queryLocalOrder(String id, String orderNo);
+
+    R createAndPrepayOrder(String id, String type);
+
+    void updateLocalOrderStatus(String orderNo, OrderStatus orderStatus);
 }
